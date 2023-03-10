155a262928600bfea8f6e9dcd32a4afce2dbfa8e
Ifeanyi Ikefuna <iikefuna@parkwayprojects.com>
2023-03-10 13:38:50 +0100

    first commit

---
+++ QuilgoConsoleTestApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace QuilgoConsoleTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveDuplicates(new int[]{1,1,2,2,3,4,5,5 }));
+            Console.ReadLine();
+        }
+
+        static int[] RemoveDuplicates(int[] nums)
+        {
+            HashSet<int> distinct = new HashSet<int>();
+            
+            foreach (int n in nums)
+            {
+                distinct.Add(n);
+            }
+
+            int[] result = new int[distinct.Count];
+            distinct.CopyTo(result);
+            return result;
+        }
+    }
+}
