7d89b4728785d753eb989cc3752e1e9f82e7eca7
Ifeanyi Ikefuna <iikefuna@parkwayprojects.com>
2023-03-10 14:43:45 +0100

    Finalize test

---
+++ TestProject/Tests.cs
@@ -0,0 +1,22 @@
+using QuilgoConsoleTestApp;
+
+namespace TestProject
+{
+    public class Tests
+    {
+
+        [Test]
+        public void NumberWithHighestOccurenceRate_TestIsCorrect()
+        {
+            //arrange
+            int[] numbers = { 1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 };
+            int rightAnswer = 5;
+
+            //act
+            int result = Solution.NumberWithHighestOccurenceRate(numbers);
+
+            //assert
+            Assert.AreEqual(rightAnswer, result);
+        }
+    }
+}
